7f65c5f53a6ade061ca10730d7f24ab01d37b65f
NoNormalDev <d.jochymek12@gmail.com>
2025-02-21 15:26:56 +0100

    Extra

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -1,11 +1,20 @@
 extends Camera3D
 
+@export var move_speed: float = 5.0
+@export var look_sensitivity: float = 0.004
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var velocity := Vector3.ZERO
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(event):
+	# Mouse Look
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * look_sensitivity)
+		var x_rotation = -event.relative.y * look_sensitivity
+		var new_rotation = rotation_degrees.x + rad_to_deg(x_rotation)
+		rotation_degrees.x = clamp(new_rotation, -90, 90)
+
 func _process(delta: float) -> void:
-	pass
+	position.y += Input.get_axis("down","up")*delta*5.0
---
+++ scripts/lane.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+var rodanimation: float = 0.0
+
+func updaterod() -> void:
+	
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
